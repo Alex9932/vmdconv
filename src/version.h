99592a314cb044fe464627a6187a8a75954974be
Alex9932 <a.l.e.x.76548@gmail.com>
2026-05-14 05:02:22 +0300

    external file for version

---
+++ src/version.h
@@ -0,0 +1,20 @@
+﻿#ifndef _VERSION_H
+#define _VERSION_H
+
+static const char* CODE_NAMES[] = {
+	"Perlica",    // v1.0 ⚡
+	// For future use
+	"Chen",       // v1.1 ⚔️
+	"DaPan",      // v1.2 🥟
+	"Endmin",     // v1.3 👨‍💻
+};
+
+#define VERSION  "1.0"
+#define CODENAME CODE_NAMES[0]
+#define BUILD    "14.05.2026"
+
+static void BuildVersionString(char* buffer, size_t bufferSize) {
+	SDL_snprintf(buffer, bufferSize, "Version %s \"%s\" (build %s)", VERSION, CODENAME, BUILD);
+}
+
+#endif
